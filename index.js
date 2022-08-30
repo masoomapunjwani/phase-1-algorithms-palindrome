ea5e865aab931d95f7c1e6a642b266ddd4a7196b
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-30 23:25:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reservedWord = word.split('').reverse().join('');
+  return word === reservedWord;
+  
 }
 
 /* 
   Add your pseudocode here
+  write a function that takes a word as argument
+    split the word to its letters, reverse the resulting array and join the letters
+    store the result in a variable
+    comapre the reserved word and the input argument
+    return the output of the comparison
+
 */
 
 /*
   Add written explanation of your solution here
+  first i need to write a function that takes one argument a word, 
+  split to its letters. and then reverse the resulting array and
+  join the letters and then i need to store the result in to that variable,
+  after that i need to compare the result and the input argument
+  and return the report.
 */
 
 // You can run `node index.js` to view these console logs
